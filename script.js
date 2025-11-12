2b209f39450fdb4fd0a57d22504ebf6f9ba5792a
Rahul <rahulofficial7652@gmail.com>
2025-11-12 22:35:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
 
-  //your code here
+  let total = 0;
 
+  for (let i = 0; i < roman.length; i++) {
+    const current = map[roman[i]];
+    const next = map[roman[i + 1]];
+
+    // If current value is smaller than next, subtract it
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
